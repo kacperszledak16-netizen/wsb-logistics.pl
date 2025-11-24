a48918430201a58e69d0a4fd6a172c0cf4ebe2ef
WSB <kacper.szledak16@gmail.com>
2025-11-24 19:49:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+    document.addEventListener("DOMContentLoaded", () => {
+
+    /* DARK MODE */
+    const slider = document.getElementById("darkmode-slider");
+    const icon = slider.querySelector(".icon");
+
+    // Jeśli brak zapisu w localStorage → ustaw domyślnie dark
+    if(!localStorage.getItem("theme")){
+        localStorage.setItem("theme", "dark");
+    }
+
+    // Ustawienie motywu na podstawie zapisu
+    if(localStorage.getItem("theme") === "dark"){
+        document.body.classList.add("dark");
+        slider.classList.add("dark");
+        icon.textContent = "☀️";
+        icon.style.left = "calc(100% - 22px)";
+    } else {
+        icon.textContent = "🌙";
+        icon.style.left = "2px";
+    }
+
+    // kliknięcie suwaka
+    slider.addEventListener("click", () => {
+        document.body.classList.toggle("dark");
+        slider.classList.toggle("dark");
+
+        if(document.body.classList.contains("dark")){
+            icon.textContent = "☀️";
+            icon.style.left = "calc(100% - 22px)";
+            localStorage.setItem("theme", "dark");
+        } else {
+            icon.textContent = "🌙";
+            icon.style.left = "2px";
+            localStorage.setItem("theme", "light");
+        }
+    });
+
+    /* MOBILE MENU */
+    document.querySelector(".menu-toggle").addEventListener("click", ()=>{
+        document.querySelector(".nav-links").classList.toggle("open");
+    });
+
+    /* LIGHTBOX */
+    const gallery = document.querySelectorAll(".gallery-grid img");
+    const lightbox = document.getElementById("lightbox");
+    const lightboxImg = lightbox.querySelector("img");
+
+    gallery.forEach(img => {
+        img.addEventListener("click", ()=>{
+            lightbox.classList.add("active");
+            lightboxImg.src = img.src;
+        });
+    });
+
+    lightbox.addEventListener("click", ()=>{
+        lightbox.classList.remove("active");
+    });
+
+});
